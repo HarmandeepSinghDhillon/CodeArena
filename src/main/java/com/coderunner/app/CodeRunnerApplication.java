911b78115ac7f620e993524bdc07bf5d00e9c359
HarmandeepSinghDhillon <harmandeep.singh233@lpu.in>
2026-07-06 16:14:51 +0530

    fixed the ipv6 addressing problem in render

--- src/main/java/com/coderunner/app/CodeRunnerApplication.java
+++ src/main/java/com/coderunner/app/CodeRunnerApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CodeRunnerApplication {
     public static void main(String[] args) {
+        System.setProperty("java.net.preferIPv4Stack", "true");
+        System.setProperty("java.net.preferIPv6Addresses", "false");
         SpringApplication.run(CodeRunnerApplication.class, args);
     }
 }
